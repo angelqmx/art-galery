ec4d3ccd55e9e7211dc2a0219575805927c4b8ca
Angel Quijano <angel.quijanomx@gmail.com>
2018-03-13 09:59:51 -0600

    ip

--- truffle.js
+++ truffle.js
@@ -3,7 +3,7 @@ module.exports = {
   // to customize your Truffle configuration!
   networks: {
     development: {
-      host: "localhost",
+      host: "35.172.153.129",
       port: 7545,
       network_id: "*"
     }
